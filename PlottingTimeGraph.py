6f5f2820a083f362986df35e492069f3dd8a0f76
Sneha <sneha46749@gmail.com>
2020-08-26 23:50:56 +0530

    Plotting the object detection times on graph using the bokeh library of Python

--- PlottingTimeGraph.py
+++ PlottingTimeGraph.py
@@ -5,14 +5,16 @@ from bokeh.models import HoverTool, ColumnDataSource
 df["Start_string"]=df["Start"].dt.strftime("%Y-%m-%d  %H:%M:%S")
 df["End_string"]=df["End"].dt.strftime("%Y-%m-%d  %H:%M:%S")
 
-cds = ColumnDataSource(df)
+cds = ColumnDataSource(df)  #To extract the column data from the source file
 
 p=figure(x_axis_type="datetime",height="100",width="500",responsive=True,title="Motion Graph")
 p.yaxis.minor_tick_line_color=None  
 p.ygrid[0].ticker.desired_num_ticks=1
 
+"""HoverTool is used to display the information on any graph simply as a popup window"""
 hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
-q=p.quad(left=["Start"],right=["End"],bottom=0,top=1,color="greaan")
+
+q=p.quad(left=["Start"],right=["End"],bottom=0,top=1,color="green")
 
 output_file("Motion_Graph.html")
 show(p)
